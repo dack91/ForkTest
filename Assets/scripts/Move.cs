f8aebbaddf571f2e8f1dc90803501b9701e0399a
Delainey Ackerman <dack91@gmail.com>
2017-05-23 14:49:40 +0100

    remove automerge messages left in files

--- Assets/scripts/Move.cs
+++ Assets/scripts/Move.cs
@@ -14,15 +14,12 @@ public class Move : MonoBehaviour {
         trans = obj.GetComponent<Transform>();
         locX = trans.position.x;
 	}
-<<<<<<< HEAD
 
 	// 3rd comment
 	// 2nd comment
 	// asfkawseifvojwsvi
-=======
 	
     // comment in conflicting line with artemis' change
->>>>>>> 6692f71e6866a553038675c0a69e0e45c9dfcddd
 	// Update is called once per frame
 	void Update () {
         locX = Mathf.Cos(Time.time * 10);            // change from sin to cos and speed up movement
