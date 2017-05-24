fa1c295fb3873f34c41c91c1577e774b7324aeb5
Delainey Ackerman <dack91@gmail.com>
2017-05-24 09:17:55 +0100

    add extra folders, modify prefab and scene

---
+++ Assets/FolderForkDoesntNeed/B02.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B02 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//blarg comment 01
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
